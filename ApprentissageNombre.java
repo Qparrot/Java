c07e3b509f9a489c1bfa016d4dd624943df45e97
Qparrot <parrot.quentin@gmail.com>
2018-05-23 17:51:48 +0200

    Add first commit

---
+++ ApprentissageNombre.java
@@ -0,0 +1,16 @@
+public class ApprentissageNombre
+{
+	// @param
+	public static void main(String[] argc)
+	{
+	int	i;
+	float	j;
+	boolean	k;
+	char	l;
+
+	j = 5f;
+	i = 5 * (int)j;
+	// TODO
+	System.out.print(i);
+	}
+}
